0179528545501ec6e692865c409f2675cb77c8e6
Lepetitdev <jerem71100@gmail.com>
2017-09-07 18:32:53 +0200

    docker

--- docker.sh
+++ docker.sh
@@ -2,5 +2,11 @@
 docker pull phpmyadmin/phpmyadmin
 docker pull mysql/mysql-server
 docker pull ubuntu
+cd /usr/ubuntu
+git clone https://github.com/cabri89/DevOpsCount.git
+cd DevOpsCount
 docker build -f nginx -t nginx .
-docker pull grafana/grafana
+git clone https://github.com/stefanprodan/dockprom.git
+docker-compose up & > web.logs
+cd dockprom
+docker-compose up & > graphana.logs
